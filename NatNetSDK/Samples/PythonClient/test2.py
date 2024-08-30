35a2196b457ec4ebc7d3ec32b9ffb8df4413fd7a
NoamGon <noamgon1996@gmail.com>
2024-08-30 11:05:11 +0300

    better organization

--- NatNetSDK/Samples/PythonClient/test2.py
+++
@@ -1,5 +0,0 @@
-import natnetclient as natnet
-
-client = natnet.NatClient(client_ip="192.168.0.181", server_ip="192.168.0.162", data_port=1511, comm_port=1510)
-bot = client.rigid_bodies["Bot"]
-print(bot.position)
